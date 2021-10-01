acc093ed03416bd7fa4ae560893ae284bfeb82a4
Ralph Royeen Lagumen <mstream64@gmail.com>
2021-10-01 10:39:46 +0800

    adding press effects on card

--- src/components/card/ProductCard.js
+++ src/components/card/ProductCard.js
@@ -22,43 +22,63 @@ const ProductCard = ({ data, navigation }) => {
         })
       }
     >
-      <Box shadow="4" rounded="25" w="64" m="2" overflow="hidden">
-        <Box>
-          <AspectRatio ratio={16 / 11}>
-            <Image
-              source={{
-                uri: data.img,
-              }}
-              alt={`${data.name}`}
-            />
-          </AspectRatio>
-        </Box>
-        <VStack w="100%" bgColor="warmGray.50" pb="4" px="4">
-          <Heading size="md" my="2" color="primary.800" isTruncated>
-            {data.name}
-          </Heading>
+      {({ isHovered, isFocused, isPressed }) => {
+        return (
+          <Box
+            shadow={isPressed ? "0" : "4"}
+            rounded="25"
+            w="64"
+            m="2"
+            overflow="hidden"
+            style={{
+              transform: [
+                {
+                  scale: isPressed ? 0.96 : 1,
+                },
+              ],
+            }}
+          >
+            <Box>
+              <AspectRatio ratio={16 / 11}>
+                <Image
+                  source={{
+                    uri: data.img,
+                  }}
+                  alt={`${data.name}`}
+                />
+              </AspectRatio>
+            </Box>
+            <VStack w="100%" bgColor="warmGray.50" pb="4" px="4">
+              <Heading size="md" my="2" color="primary.800" isTruncated>
+                {data.name}
+              </Heading>
 
-          <HStack my="1" justifyContent="space-between" alignItems="center">
-            <Text bold>
-              {data.ratings}
-              <Text sub> / 5.0</Text>
-            </Text>
-            <MenuCategoryBadge category={data.category} label={data.category} />
-          </HStack>
+              <HStack my="1" justifyContent="space-between" alignItems="center">
+                <Text bold>
+                  {data.ratings}
+                  <Text sub> / 5.0</Text>
+                </Text>
+                <MenuCategoryBadge
+                  category={data.category}
+                  label={data.category}
+                />
+              </HStack>
 
-          <Text color={data.available ? "darkText" : `danger.600`}>
-            {data.available ? `Available` : `Not Available`}
-          </Text>
+              <Text color={data.available ? "darkText" : `danger.600`}>
+                {data.available ? `Available` : `Not Available`}
+              </Text>
 
-          <Divider my="2" w="full" />
-          <HStack alignSelf="flex-end">
-            <Text sub>PHP </Text>
-            <Text bold fontSize="2xl">
-              {data.price}
-            </Text>
-          </HStack>
-        </VStack>
-      </Box>
+              <Divider my="2" w="full" />
+              <HStack alignSelf="flex-end">
+                <Text sub>PHP </Text>
+                <Text bold fontSize="2xl">
+                  {data.price}
+                </Text>
+              </HStack>
+            </VStack>
+          </Box>
+        );
+      }}
     </Pressable>
   );
 };
